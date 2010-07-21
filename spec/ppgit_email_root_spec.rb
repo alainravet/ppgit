b3d957dfb68a8b0f47970d9cdd83384df07db6fc
Alain Ravet <alainravet+git@gmail.com>
2010-07-21 18:50:13 +0200

    combining pair names/email and --email_root in 1 line
    ex: 
      ppgit john andy --email_root ppatvasco+*@gmail.com

--- spec/ppgit_email_root_spec.rb
+++ spec/ppgit_email_root_spec.rb
@@ -24,3 +24,43 @@ describe "`ppgit --email_root acme+*@gmail.com`" do
     check_command_result(@cmd, before, expected)
   end
 end
+
+describe "`ppgit john andy --email_root acme+*@gmail.com`" do
+  before(:all) do
+    @cmd = "#{PPGIT_CMD} john andy --email_root acme+*@gmail.com"
+  end
+
+  it 'generates an email for the pair from the root AND stores the email_root' do
+    before   = [  '[z]'                 ]
+
+    expected = [  '[z]'                             ,
+                  '[ppgit]'                         ,
+                  '  emailroot = acme+*@gmail.com'  ,
+                  '[user]'                          ,
+                  '  name = andy_john'              ,
+                  '  email = acme+andy_john@gmail.com' ,
+                ]
+
+    check_command_result(@cmd, before, expected)
+  end
+end
+
+describe "`ppgit john andy andy_john@test.com --email_root acme+*@gmail.com`" do
+  before(:all) do
+    @cmd = "#{PPGIT_CMD} john andy andy_john@test.com --email_root acme+*@gmail.com"
+  end
+
+  it 'stores the email_root BUT uses the explicit pair email address' do
+    before   = [  '[z]'                 ]
+
+    expected = [  '[z]'                             ,
+                  '[ppgit]'                         ,
+                  '  emailroot = acme+*@gmail.com'  ,
+                  '[user]'                          ,
+                  '  name = andy_john'              ,
+                  '  email = andy_john@test.com' ,
+                ]
+
+    check_command_result(@cmd, before, expected)
+  end
+end
