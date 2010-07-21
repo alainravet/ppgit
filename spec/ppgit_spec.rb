cf26093a4695ba47f231c16c1b0734941ba1a583
a_b_c <alain.ravet+git@gmail.com>
2010-07-21 12:29:39 +0200

    `ppgit john andy andy_john@test.email` when there is no user.email yet.

--- spec/ppgit_spec.rb
+++ spec/ppgit_spec.rb
@@ -5,28 +5,50 @@ describe "`ppgit john andy`" do
   context 'when there is no user name nor email nor ppgit.email.root in the config' do
     before = [    '[a]'                 ]
 
-    expected = [  '[a]'                 ,
-                  '[user]'              ,
-                  '  name = andy_john'  ]
-
     it "combines the names alphabetically and stores them a the new user.name" do
+      expected = [  '[a]'                 ,
+                    '[user]'              ,
+                    '  name = andy_john'  ]
+
       cmd = "#{PPGIT_CMD} john andy"
+      check_command_result(cmd, before, expected)
+    end
+
+    it "uses the 3rd parameter as new user.email" do
+      expected = [  '[a]'                           ,
+                    '[user]'                        ,
+                    '  name = andy_john'            ,
+                    '  email = andy_john@test.com'  ]
+
+      cmd = "#{PPGIT_CMD} john andy andy_john@test.com"
       check_command_result(cmd, before, expected)
     end
   end
 
 
-  context 'when there is a user name but no ppgit.email.root' do
+  context 'when there is a user.name' do
     before = [    '[user]'                ,
                   '  name = Alain Ravet'  ]
 
-    expected = [  '[user]'                ,
-                  '  name = andy_john'    ,
-                  '[user-before-ppgit]'   ,
-                  '  name = Alain Ravet'  ]
 
     it "stores the existing user.name and sets the pair user as the new user.name" do
+      expected = [  '[user]'                ,
+                    '  name = andy_john'    ,
+                    '[user-before-ppgit]'   ,
+                    '  name = Alain Ravet'  ]
+
       cmd = "#{PPGIT_CMD} john andy"
+      check_command_result(cmd, before, expected)
+    end
+
+    it "uses the 3rd parameter as new user.email" do
+      expected = [  '[user]'                        ,
+                    '  name = andy_john'            ,
+                    '  email = andy_john@test.com'  ,
+                    '[user-before-ppgit]'           ,
+                    '  name = Alain Ravet'          ]
+
+      cmd = "#{PPGIT_CMD} john andy andy_john@test.com"
       check_command_result(cmd, before, expected)
     end
   end
