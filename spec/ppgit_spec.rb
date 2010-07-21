32f54d4a3d8b46f3c46b41ce89a0fb8bebb5afce
a_b_c <alain.ravet+git@gmail.com>
2010-07-21 11:57:59 +0200

    store the existing value of 'user.name' in 'user-before-ppgit.name'

--- spec/ppgit_spec.rb
+++ spec/ppgit_spec.rb
@@ -3,16 +3,29 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "`ppgit john andy`" do
 
   context 'when there is no user name nor email nor ppgit.email.root in the config' do
-    before = [
-      '[a]'                 ,
-    ]
-    expected = [
-      '[a]'                 ,
-      '[user]'              ,
-      '  name = andy_john'  ,
-    ]
+    before = [    '[a]'                 ]
+
+    expected = [  '[a]'                 ,
+                  '[user]'              ,
+                  '  name = andy_john'  ]
 
     it "combines the names alphabetically and stores them a the new user.name" do
+      cmd = "#{PPGIT_CMD} john andy"
+      check_command_result(cmd, before, expected)
+    end
+  end
+
+
+  context 'when there is a user name but no ppgit.email.root' do
+    before = [    '[user]'                ,
+                  '  name = Alain Ravet'  ]
+
+    expected = [  '[user]'                ,
+                  '  name = andy_john'    ,
+                  '[user-before-ppgit]'   ,
+                  '  name = Alain Ravet'  ]
+
+    it "stores the existing user.name and sets the pair user as the new user.name" do
       cmd = "#{PPGIT_CMD} john andy"
       check_command_result(cmd, before, expected)
     end
